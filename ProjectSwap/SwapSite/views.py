954f648bb3928b12b8f898be16f586d621d520ae
Eirik Pedersen <eirik.sp.2002@gmail.com>
2019-11-09 12:30:10 +0100

    Set up Templates and made basic design changes

--- ProjectSwap/SwapSite/views.py
+++ ProjectSwap/SwapSite/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+UserPages = [
+    {
+        'Seller':'John',
+        'Item':'Bike',
+        'Condition':'Mostly New',
+        'Location':'Paris',
+        'time_posted':'26th. August 2019'
+    },
+    {
+        'Seller':'Carrie',
+        'Item':'Sofa',
+        'Condition':'Old',
+        'Location':'Berlin',
+        'time_posted':'10th. July 2019'
+    }
+
+]
+
+
 def home(request):
-    return render(request, 'SwapSite/home.html')
+    context = {
+        'UserPages':UserPages,
+        'title':'Home'
+    }
+    return render(request, 'SwapSite/home.html', context)
 
 def about(request):
-    return render(request, 'SwapSite/about.html')
+    return render(request, 'SwapSite/about.html', {'title':'About Page'})
